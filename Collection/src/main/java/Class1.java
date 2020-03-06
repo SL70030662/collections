8c09bcb76737ab71f3d9466364efaa7f037e003d
Akhil <Akhil@LAPTOP-3O8A24HK>
2020-03-06 15:53:29 +0530

    collections

---
+++ Collection/src/main/java/Class1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Class1 {
+
+	private static final Logger logger = LogManager.getLogger(Class1.class);
+    private static Scanner scan;
+
+	public static void main(String[] args) 
+    {
+    	scan = new Scanner(System.in);
+    	Class2<Integer> list = new Class2<Integer>();
+    	logger.info("Initial list:");
+    	logger.info(list);
+        int noOfAdd=scan.nextInt();
+        logger.info("enter elements:");
+        for (int i=0;i<noOfAdd;i++)
+        {
+        	int adding=scan.nextInt();
+        	list.add(adding);
+        }
+        logger.info("Total list after adding elements:");
+        logger.info(list);
+        logger.info("enter index to be removed");
+        int removeElement;
+        removeElement=scan.nextInt();
+        list.remove(removeElement);
+        logger.info("Total list after removing elements:"+list);
+         
+
+        logger.info("value at index 0 is "+ list.fetch(0));
+
+        logger.info("value at index 1 is:"+list.fetch(1));
+ 
+        //List Size
+        logger.info("total size"+list.s());
+    }
+}
